dc49f892b8455169a5c5d1c4226864a36ff63b19
wellington <wellingtonnunesverzola@gmail.com>
2022-06-21 18:02:06 -0300

    Update

---
+++ js/wnv.js
@@ -0,0 +1,43 @@
+function calcularIdade(data) {
+    var now = new Date();
+    var today = new Date(now.getYear(), now.getMonth(), now.getDate());
+
+    var yearNow = now.getYear();
+    var monthNow = now.getMonth();
+    var dateNow = now.getDate();
+    var dob = new Date(
+        data.substring(6, 10),
+        data.substring(3, 5) - 1,
+        data.substring(0, 2)
+    );
+
+    var yearDob = dob.getYear();
+    var monthDob = dob.getMonth();
+    var dateDob = dob.getDate();
+    var age = {};
+    yearAge = yearNow - yearDob;
+
+    if (monthNow >= monthDob) var monthAge = monthNow - monthDob;
+    else {
+        yearAge--;
+        var monthAge = 12 + monthNow - monthDob;
+    }
+
+    if (dateNow >= dateDob) var dateAge = dateNow - dateDob;
+    else {
+        monthAge--;
+        var dateAge = 31 + dateNow - dateDob;
+
+        if (monthAge < 0) {
+            monthAge = 11;
+            yearAge--;
+        }
+    }
+
+    age = {
+        years: yearAge,
+        months: monthAge,
+        days: dateAge
+    };
+    document.getElementById('idade').innerHTML = age.years;
+}
