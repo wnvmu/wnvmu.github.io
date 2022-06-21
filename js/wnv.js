005ce085f0de6d9954d436915e9fe5e4052f093d
wellington <wellingtonnunesverzola@gmail.com>
2022-06-21 20:22:19 -0300

    Update

--- js/wnv.js
+++ js/wnv.js
@@ -1,7 +1,6 @@
 function calcularIdade(data) {
     var now = new Date();
     var today = new Date(now.getYear(), now.getMonth(), now.getDate());
-
     var yearNow = now.getYear();
     var monthNow = now.getMonth();
     var dateNow = now.getDate();
@@ -10,19 +9,16 @@ function calcularIdade(data) {
         data.substring(3, 5) - 1,
         data.substring(0, 2)
     );
-
     var yearDob = dob.getYear();
     var monthDob = dob.getMonth();
     var dateDob = dob.getDate();
     var age = {};
     yearAge = yearNow - yearDob;
-
     if (monthNow >= monthDob) var monthAge = monthNow - monthDob;
     else {
         yearAge--;
         var monthAge = 12 + monthNow - monthDob;
     }
-
     if (dateNow >= dateDob) var dateAge = dateNow - dateDob;
     else {
         monthAge--;
@@ -33,7 +29,6 @@ function calcularIdade(data) {
             yearAge--;
         }
     }
-
     age = {
         years: yearAge,
         months: monthAge,
@@ -49,4 +44,43 @@ function disponivel() {
     } else {
         document.getElementById('trabalho').innerHTML = "<div style='color:rgb(42, 240, 2);'>Disponível</div>";
     }
+}
+
+function temposervico(data) {
+    var now = new Date();
+    var today = new Date(now.getYear(), now.getMonth(), now.getDate());
+    var yearNow = now.getYear();
+    var monthNow = now.getMonth();
+    var dateNow = now.getDate();
+    var dob = new Date(
+        data.substring(6, 10),
+        data.substring(3, 5) - 1,
+        data.substring(0, 2)
+    );
+    var yearDob = dob.getYear();
+    var monthDob = dob.getMonth();
+    var dateDob = dob.getDate();
+    var age = {};
+    yearAge = yearNow - yearDob;
+    if (monthNow >= monthDob) var monthAge = monthNow - monthDob;
+    else {
+        yearAge--;
+        var monthAge = 12 + monthNow - monthDob;
+    }
+    if (dateNow >= dateDob) var dateAge = dateNow - dateDob;
+    else {
+        monthAge--;
+        var dateAge = 31 + dateNow - dateDob;
+
+        if (monthAge < 0) {
+            monthAge = 11;
+            yearAge--;
+        }
+    }
+    age = {
+        years: yearAge,
+        months: monthAge,
+        days: dateAge
+    };
+    document.getElementById('servico').innerHTML = age.years;
 }
