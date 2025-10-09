8b61676c063530f499c953a341da4c17c0488304
Wellington N. Verzola <wellingtonnunesverzola@gmail.com>
2025-10-09 20:33:01 -0300

    Delete financeiro/preload.js

--- financeiro/preload.js
+++
@@ -1,6 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('fsAPI', {
-  login: (user, pass, createIfMissing) => ipcRenderer.invoke('login', {user, pass, createIfMissing}),
-  save: (csv) => ipcRenderer.invoke('save', {csv})
-});
